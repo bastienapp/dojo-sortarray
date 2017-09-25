70cc23c9b9875f2a9c06e4de443e23d6d3214a86
bastienwcs <bastien@wildcodeschool.fr>
2017-09-25 19:06:17 +0200

    Ajout d'un test d'exemple

--- SortArrayTest.java
+++ SortArrayTest.java
@@ -1,9 +1,17 @@
 import junit.framework.*;
 import org.junit.Test;
+import org.junit.Assert;
 
 public class SortArrayTest extends TestCase {
 	@Test
 	public void testTrue() throws Exception {
 		assertEquals(true, true);
+	}
+
+	@Test
+	public void test1() throws Exception {
+		int[] arrayToSort = new int[]{0,1};
+		int[] arraySorted = new int[]{0,1};
+		Assert.assertArrayEquals(SortArray.sort(arrayToSort), arraySorted);
 	}
 }
